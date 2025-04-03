355349c45c3a640d0446f58183d0a7289974fd2a
CaMoraG <camoraq83@gmail.com>
2025-04-03 14:44:08 -0500

    fix on oracle db

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [ ! -f docker/.env ]; then
 fi
 
 # Load environment variables
-envsubst < docker/database/setup/schema.sql.template > docker/database/setup/schema.sql
+env $(grep -v '^#' docker/.env | xargs) envsubst < docker/database/setup/schema.sql.template > docker/database/setup/schema.sql
 
 # Build and start services
 cd docker
